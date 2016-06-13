35de5c5db69fcbaece482a72d2de1a7701e244e2
Joey Dingcong <joeydngcng1231@gmail.com>
2016-06-13 13:10:54 +0800

    Creating the ionic first intallation from ioni.creator

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('contactsCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+      
+.controller('tab4DefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('aDDCONTACTCtrl', function($scope) {
+
+})
+   
+.controller('loginCtrl', function($scope) {
+  
+})
+ 
