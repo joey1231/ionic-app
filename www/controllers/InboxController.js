59a09d6b578fe7ef8769d1ec85b0793ab88e61ec
Mark Kevin Besinga <besingamkb@gmail.com>
2016-06-14 18:29:02 +0800

    conversation message
    display of messages by direction
    display thread recipients as a title
    display diff of the time send from the current

--- www/controllers/InboxController.js
+++ www/controllers/InboxController.js
@@ -1,9 +1,12 @@
-controllers.inboxCtrl = function($scope, $http, baseUrl) {
+controllers.inboxCtrl = function($scope, $http, $stateParams, baseUrl) {
+
+
+	$scope.user = JSON.parse(window.localStorage.getItem('user'));
+
 	
 	$scope.inbox = function() {
 
 		$scope.inbox = new Array();
-		$scope.user = JSON.parse(window.localStorage.getItem('user'));
 
 		$http.get(baseUrl + '/communication/inbox', {params:{userid:$scope.user.id}}).success(function (data, status, headers) {
             
@@ -16,6 +19,29 @@ controllers.inboxCtrl = function($scope, $http, baseUrl) {
 	}
 
 	$scope.inbox();
+
+
+
+	$scope.converstaion = function() {
+
+
+		$scope.conversations = new Array();
+
+		$http.get(baseUrl + '/communication/exchange/' + $stateParams.thread_key, {params:{userid:$scope.user.id}}).success(function(data, status, header) {
+			
+
+			$scope.conversations =data.data.conversations;
+			var title = "";
+
+			angular.forEach($scope.getRecipients(data.data.recipients), function(d, k) {
+				title += d + ", ";
+			});
+			
+			$scope.title = title.substring(0, title.length - 2)
+		});
+
+
+	}
 
 	$scope.getRecipients = function(recipient) {
 		// if (typeof x != 'undefined' && x instanceof Array)
--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('app', ['ionic', 'app.routes', 'app.services', 'app.directives'])
+var app = angular.module('app', ['ionic', 'app.routes', 'app.services', 'app.directives', 'angularMoment'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
