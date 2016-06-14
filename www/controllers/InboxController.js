67fda0e0d004f77fe1ee33af68fe06c23bf01b83
Mark Kevin Besinga <besingamkb@gmail.com>
2016-06-14 10:45:09 +0800

    initialize inbox data

--- www/controllers/InboxController.js
+++ www/controllers/InboxController.js
@@ -1,3 +1,20 @@
-controllers.inboxCtrl = function($scope) {
+controllers.inboxCtrl = function($scope, $http, baseUrl) {
 	
+	$scope.inbox = function() {
+
+		$scope.inbox = new Array();
+		$scope.user = JSON.parse(window.localStorage.getItem('user'));
+
+		$http.get(baseUrl + '/communication/inbox', {params:{userid:$scope.user.id}}).success(function (data, status, headers) {
+            
+            if (status == 200) {
+                console.log(data);
+            	console.log($scope.user.id);
+            } else {
+                //alert("Something went wrong!");
+            }
+        });
+	}
+
+	$scope.inbox();
 }
