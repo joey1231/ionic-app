067a937d6156ccbf4f76eeec3207cdd099d04afa
Mark Kevin Besinga <besingamkb@gmail.com>
2016-06-15 10:59:40 +0800

    fixing contacts bugs

--- www/controllers/ContactController.js
+++ www/controllers/ContactController.js
@@ -1,11 +1,11 @@
 controllers.contactsCtrl = function($scope, $http, baseUrl){
 	$scope.init = function(){
 
-        $scope.contacts = new Array();
-        $scope.user = JSON.parse(window.localStorage.getItem('user'));
+        
 
         $http.get(baseUrl + '/contact', { params:{ userid: $scope.user.id } }).success(function (data, status, headers) {
-
+            $scope.contacts = new Array();
+            $scope.user = JSON.parse(window.localStorage.getItem('user'));
             if (status == 200) {
                 $scope.contacts = data;
                 //$scope.totalData = $scope.contacts.length;
