dd6dc5c00ee0fc81926edb74096313fc710ec7f2
Joey Dingcong <joeydngcng1231@gmail.com>
2016-06-21 18:07:03 +0800

    change for setting page

--- www/controllers/SettingController.js
+++ www/controllers/SettingController.js
@@ -1,3 +1,17 @@
-controllers.settingCtrl = function($scope) {
-	
+controllers.settingCtrl = function($scope,$http, baseUrl, $timeout, $q, $ionicPopup,$state,$stateParams) {
+    $scope.user = JSON.parse(window.localStorage.getItem('user'));
+    $scope.profile=new Array();
+    $scope.profile.forwarding_devices= new Array();
+    $scope.init = function () {
+        // Make an get request to fetch the data to server
+        $http.get(
+            baseUrl + "/dashboard/profile", { params:{ userid: $scope.user.id } }
+        ).success(function (data) {
+
+            $scope.profile = data;
+            console.log(data);
+        }).error(function (data, status, header, config) {
+        });
+
+    }
 }
