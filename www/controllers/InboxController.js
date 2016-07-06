19212aab660674d9ae706b4469d47739e2c6ff22
Mark Kevin Besinga <besingamkb@gmail.com>
2016-07-06 08:33:45 +0800

    upload function

--- www/controllers/InboxController.js
+++ www/controllers/InboxController.js
@@ -10,7 +10,8 @@ controllers.inboxCtrl = function(
     $ionicActionSheet,
     $cordovaToast,
     ScaleDronePush,
-    ScaleDroneService) {
+    ScaleDroneService,
+    Upload) {
 
     $scope.input = {
         message: ""
@@ -335,7 +336,27 @@ controllers.inboxCtrl = function(
     /*
      * upload file function
      */
-    $scope.initUpload = function() {
-        
+    $scope.uploadFiles = function() {
+        $scope.uploadFiles = function(file, errFiles) {
+        $scope.f = file;
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+            file.upload = Upload.upload({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                data: {file: file}
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                });
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            });
+        }   
+    }
     }
 };
