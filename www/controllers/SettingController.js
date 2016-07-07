cd0cdd20c82eb1b4abd34054298f74c7b3e9db88
Joey Dingcong <joeydngcng1231@gmail.com>
2016-07-07 10:03:01 +0800

    updating setting file

--- www/controllers/SettingController.js
+++ www/controllers/SettingController.js
@@ -9,11 +9,32 @@ controllers.settingCtrl = function($scope, $http, $timeout, $q, $ionicPopup, $st
         ).success(function(data) {
             console.log(data);
             $scope.profile = data;
-
+            $scope.proccessToggle();
         }).error(function(data, status, header, config) {
             console.log(data);
 
         });
 
+    }
+
+    $scope.toggle_setting ={
+        'phone_webrtc_enabled':false,
+        'forward_incoming_message':false,
+        'sound_notification':false,
+        'desktop_notification':false,
+        'hide_link_bulk_message':false,
+        'disable_incoming_message':false,
+        'transcribe_voicemail':false,
+    }
+    $scope.proccessToggle=function(){
+        $scope.toggle_setting ={
+        'phone_webrtc_enabled':$scope.profile.phone_webrtc_enabled == 1 ? true : false,
+        'forward_incoming_message':$scope.profile.forward_incoming_message == 1 ? true : false,
+        'sound_notification':$scope.profile.sound_notification == 1 ? true:false,
+        'desktop_notification':$scope.profile.desktop_notification == 1 ? true:false,
+        'hide_link_bulk_message':$scope.profile.hide_link_bulk_message == 1 ? true:false,
+        'disable_incoming_message':$scope.profile.disable_incoming_message == 1 ? true:false,
+        'transcribe_voicemail':$scope.profile.transcribe_voicemail == 1 ? true:false,
+         }
     }
 }
